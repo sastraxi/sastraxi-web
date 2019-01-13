be8c14305ce7101447b41a94e0e033322866b3b5
Cameron Gorrie <sastraxi@gmail.com>
2019-01-13 17:37:04 -0500

    add contact details to print version

---
+++ src/components/ContactDetails.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import Paragraph from '../elements/Paragraph';
+
+const ContactDetails = styled(Paragraph)`
+  margin-top: -0.5em;
+
+  a:after {
+    content: none;
+  }
+
+  display: none;
+  @media print {
+    display: block;
+  }
+`;
+
+export default () => (
+  <ContactDetails>
+    Email&nbsp;
+    <a href="mailto:sastraxi+resume@gmail.com">sastraxi@gmail.com</a>
+    , or call 747.275.8996
+  </ContactDetails>
+);
